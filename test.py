898b6b97f140529ed39519bf33d28cce62acd6e0
Alex Neagoe Probook <git@neagoe.email>
2023-04-17 13:24:30 +0300

    test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from yahooquery import Ticker
 
 # another test!!!
 try:
-    aapl = Ticker('1111')
+    t = Ticker('aapl')
     print(aapl.summary_profile['aapl']['longBusinessSummary'])
 except (KeyError, ValueError):
     print("Invalid input or symbol not found.")
