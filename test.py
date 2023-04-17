d968751a7ea79a0122c2047e40bbc04b81229538
Alex Neagoe <git@neagoe.email>
2023-04-17 22:59:24 +0300

    removed unecessary files

--- test.py
+++
@@ -1,9 +0,0 @@
-from yahooquery import Ticker
-
-# another test!!!
-try:
-    t = Ticker('aapl')
-    print(aapl.summary_profile['aapl']['longBusinessSummary'])
-except (KeyError, ValueError):
-    print("Invalid input or symbol not found.")
-
